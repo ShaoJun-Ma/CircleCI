09eb5dcaa667e0899cbe52e044f424c4031aac4f
msj <1751871164@qq.com>
2019-06-06 11:33:45 +0800

    CircleCI 测试

---
+++ src/test/java/TestCircleCiDemo.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestCircleCiDemo {
+    @Test
+    public void test01(){
+        Assert.assertEquals(6,6);
+        System.out.println("测试成功！");
+    }
+}
